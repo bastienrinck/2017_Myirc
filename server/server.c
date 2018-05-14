c72b45beff799c4c55565b623b960951f5084280
Bastien <bastien.rinck@epitech.eu>
2018-05-14 17:30:36 +0200

    client

--- server/server.c
+++ server/server.c
@@ -5,4 +5,22 @@
 ** Created by rectoria
 */
 
+#include <stdio.h>
+#include <string.h>
 #include "server.h"
+
+static int print_usage(char *bin)
+{
+	printf("USAGE:\t%s port\n", bin);
+	return (84);
+}
+
+int main(int ac, char **av)
+{
+	int ret = 0;
+
+	if (ac != 2 || strcmp(av[1], "-help") != 0)
+		ret = print_usage(av[0]);
+	else
+		ret = myirc_server(av[1]);
+}
